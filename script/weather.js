9a6959b3ecaf24c5bf8c5ac496b1de58549f43ee
Chris DiPiero <frogmaster99@msn.com>
2019-11-05 08:33:13 -0600

    fixed weather and increased font size

--- script/weather.js
+++ script/weather.js
@@ -2,12 +2,10 @@
 class WeatherData {
     constructor(json) {
         this.tempF = this.tempToF(json.main.temp);
-        this.lowF = this.tempToF(json.main.temp_min);
-        this.highF = this.tempToF(json.main.temp_max);
-
         this.tempC = this.tempToC(json.main.temp);
-        this.lowC = this.tempToC(json.main.temp_min);
-        this.highC = this.tempToC(json.main.temp_max);
+        this.humidity = json.main.humidity;
+        this.windSpeed = this.windMetersPSToMPH(json.wind.speed);
+        // this.windDir = this.windDegToDir(json.wind.deg); --- broken
     }
 
     tempToF(kelvin) {
@@ -16,12 +14,45 @@ class WeatherData {
     tempToC(kelvin) {
         return (kelvin - 273.15).toFixed(0);
     }
+    windMetersPSToMPH(speed) {
+        return (speed * 2.2369).toFixed(1);
+    }
+    // windDegToDir(degrees) { --- why is this broken?
+    //     let expr = parseInt(degrees);
+    //     let direction;
+    //     switch(expr) {
+    //         case (expr > 23 && expr <= 69) :
+    //             direction = "NE";
+    //             break;
+    //         case (expr <= 113) :
+    //             direction = "E";
+    //             break;
+    //         case (expr <= 158) :
+    //             direction = "SE";
+    //             break;
+    //         case ( expr <= 203) :
+    //             direction = "S";
+    //             break;
+    //         case (expr <= 248) :
+    //             direction = "SW";
+    //             break;
+    //         case (expr <= 293) :
+    //             direction = "W";
+    //             break;
+    //         case (expr <= 338) :
+    //             direction = "NW";
+    //             break;
+    //         case (expr <= 23) :
+    //             direction = "N";
+    //     }
+    //     return direction;
+    // }
 }
 
 const apiKey = '35212c2362c920fb78bcfbc661e77b86';
 
 const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=san+antonio&APPID=' + apiKey;
+    'https://api.openweathermap.org/data/2.5/weather?q=san+antonio,us&APPID=' + apiKey;
 
 $.ajax({
     url: url,
@@ -31,8 +62,10 @@ $.ajax({
     .done(function(json) {
         $("#weather").show();
         const CurrentWeather = new WeatherData(json);
-        $("<div class='col-12'>").text(`Now: ${CurrentWeather.tempF} Low: ${CurrentWeather.lowF} Hi: ${CurrentWeather.highF}`).appendTo("#degreesF");
-        $("<div class='col-12'>").text(`Now: ${CurrentWeather.tempC} Low: ${CurrentWeather.lowC} Hi: ${CurrentWeather.highC}`).appendTo("#degreesC");
+        $("<div class='col-12'>").text(CurrentWeather.tempF).appendTo("#degreesF");
+        $("<div class='col-12'>").text(CurrentWeather.tempC).appendTo("#degreesC");
+        $("<div class='col-12'>").text(`${CurrentWeather.humidity} %`).appendTo("#humidity");
+        $("<div class='col-12'>").text(`${CurrentWeather.windSpeed} mph`).appendTo("#wind");
     })
 
     .fail(function(xhr, status, errorThrown) {
